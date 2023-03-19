e6b0b57ceb2e20df94b3078f95a53e4fc9a28fd4
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-03-19 13:17:02 +0200

    - Rolled back breaking changes

--- source/libsnooze/event.d
+++ source/libsnooze/event.d
@@ -4,11 +4,18 @@ module libsnooze.event;
 // ... so I'd like libsnooze.clib to work as my IDE picks up on
 // ... it then
 
-
+version(release)
+{
+	import libsnooze.clib : pipe, write, read;
+	import libsnooze.clib : select, fd_set, fdSetZero, fdSetSet;
+	import libsnooze.clib : timeval, time_t, suseconds_t;
+}
+else
+{
 	import clib : pipe, write, read;
 	import clib : select, fd_set, fdSetZero, fdSetSet;
 	import clib : timeval, time_t, suseconds_t;
-
+}
 
 import core.thread : Thread, Duration, dur;
 import core.sync.mutex : Mutex;
--- source/libsnooze/package.d
+++ source/libsnooze/package.d
@@ -2,16 +2,3 @@ module libsnooze;
 
 public import libsnooze.event : Event;
 public import libsnooze.exceptions;
-
-// TODO: See if this fixes when importing
-// public import clib;
-// public import libsnooze
-
-mixin template ImportFix()
-{
-    import libsnooze.clib;
-    import libsnooze;
-}
-
-// Cuases build issues but atleast unit tests work
-mixin ImportFix!();
