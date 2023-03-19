caa8ee937648310230d6e40cb3f7c42f7da58e17
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-03-19 13:14:46 +0200

    Package
    - Mixin hack to get unit tests and client-side library usage working - but not OUR builds

--- source/libsnooze/package.d
+++ source/libsnooze/package.d
@@ -4,5 +4,14 @@ public import libsnooze.event : Event;
 public import libsnooze.exceptions;
 
 // TODO: See if this fixes when importing
-public import clib;
+// public import clib;
 // public import libsnooze
+
+mixin template ImportFix()
+{
+    import libsnooze.clib;
+    import libsnooze;
+}
+
+// Cuases build issues but atleast unit tests work
+mixin ImportFix!();
