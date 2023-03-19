e8ae7e67b3a04dd603cc715478b764337607403e
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-03-19 13:09:45 +0200

    Event
    - Testing new import mechanism to fix build and unit test issues
    
    Package
    
    - Testing new import mechanism

--- source/libsnooze/package.d
+++ source/libsnooze/package.d
@@ -2,3 +2,7 @@ module libsnooze;
 
 public import libsnooze.event : Event;
 public import libsnooze.exceptions;
+
+// TODO: See if this fixes when importing
+public import clib;
+// public import libsnooze
