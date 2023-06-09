8e177dd6a0919676e6d5f31870a9b0f33afe0d4e
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-06-09 10:38:35 +0200

    Exceptions
    - Added new exception type `InterruptedException` which is thrown if the call to `wait()` was interrupted for some reason

--- source/libsnooze/exceptions.d
+++ source/libsnooze/exceptions.d
@@ -1,5 +1,7 @@
 module libsnooze.exceptions;
 
+import libsnooze.event : Event;
+
 public class SnoozeError : Exception
 {
     this(string msg)
@@ -7,3 +9,29 @@ public class SnoozeError : Exception
         super(msg);
     }
 }
+/** 
+ * This exception is thrown if the call to `wait()`
+ * was interrupted for some reason
+ */
+public final class InterruptedException : SnoozeError
+{
+    private Event e;
+
+    this(Event e)
+    {
+        super("Interrupted whilst waiting on event '"~e.toString()~"'");
+        this.e = e;
+    }
+
+    public Event getEvent()
+    {
+        return e;
+    }
+}
+
+// public final class WaitException : SnoozeError
+// {
+
+// }
+
+// public final class NotifyException : SnoozeError
