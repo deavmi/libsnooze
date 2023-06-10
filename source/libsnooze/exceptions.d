37a971654f2739eb5ddcd21b2f2bb81f2fe58a20
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-06-10 14:41:25 +0200

    SnoozeError
    - Documented the `SnoozeError` class and constructor

--- source/libsnooze/exceptions.d
+++ source/libsnooze/exceptions.d
@@ -3,8 +3,18 @@
  */
 module libsnooze.exceptions;
 
+/** 
+ * The general libsnooze error type
+ */
 public class SnoozeError : Exception
 {
+    /** 
+     * Constructs a new `SnoozeError` with the provided
+     * error message
+     *
+     * Params:
+     *   msg = the error message
+     */
     package this(string msg)
     {
         super(msg);
